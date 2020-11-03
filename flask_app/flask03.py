748455c7c4e69cea65240d6de94c192e994eb6a2
Amber Sibel <asibel@uncc.edu>
2020-11-03 09:30:30 -0500

    flask app #3 - added personalized header to all views

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -22,8 +22,9 @@ def get_notes():
     2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
     3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
     }
+    a_user = {'name': 'Amber', 'email': 'asibel@uncc.edu'}
     
-    return render_template('notes.html', notes=notes)
+    return render_template('notes.html', notes=notes, user=a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
@@ -31,8 +32,9 @@ def get_note(note_id):
     2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
     3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
     }
+    a_user = {'name': 'Amber', 'email': 'asibel@uncc.edu'}
     
-    return render_template('note.html', note=notes[int(note_id)])
+    return render_template('note.html', note=notes[int(note_id)], user=a_user)
 
 @app.route('/notes/new')
 def new_note():
